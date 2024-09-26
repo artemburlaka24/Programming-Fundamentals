266d59b6bb9672b01d5bc4d8b0b3877844ff27ca
artemburlaka24 <9274798@stud.nau.edu.ua>
2024-09-26 09:05:00 +0300

    Add files via upload

---
+++ practicals/Practice1.6/Practice1.6/Source.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <bitset>
+
+int main()
+{
+	int A, count1 = 0, count2 = 0;
+	std::cout << "Task 1" << std::endl;
+	std::cout << "Write a number:" << std::endl;
+	std::cin >> A;
+	for (int i = 0; i <= A; i++)
+	{
+		if (int I = i % 2 == 0)
+		{
+			std::cout << I;
+			count2 += 1;
+		}
+		else
+		{
+			std::cout << I;
+			count1 += 1;
+		}
+	}
+	if (count1 > count2)
+	{
+		std::cout << "There's more zeros than ones" << std::endl;
+	}
+	else if (count2 > count1)
+	{
+		std::cout << "There's more ones than zeros" << std::endl;
+	}
+	else
+	{
+		std::cout << "There's the same amount of ones and zeros" << std::endl;
+	}
+	int binary1 = 0, binary2 = 0;
+	std::cout << "Task 2" << std::endl;
+	std::cout << "Write a number 1 in binary:" << std::endl;
+	std::cin >> binary1;
+	std::cout << "Write a number 2 in binary:" << std::endl;
+	std::cin >> binary2;
+	int result = binary1 ^ binary2;
+	std::cout << std::bitset<8>(result) << std::endl;
+}
